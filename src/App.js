2818417b2667efe0ec50836603d9d95ea09d2e28
akmalsyrf <dev.akmal69@gmail.com>
2021-12-14 19:26:28 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={LandingPage}/>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/ModalGroups.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Form, Button, Modal } from "react-bootstrap";
+
+function LoginModal(props) {
+    return (
+        <>
+          <Modal show={props.showLogin} onHide={props.handleCloseLogin}>
+            <Modal.Header closeButton>
+              <Modal.Title className="fw-bold">Login</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Control type="email" placeholder="Email" />
+                </Form.Group>
+    
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                  <Button variant="danger" className="col-12" onClick={props.handleCloseLogin}>
+                    Submit
+                  </Button>
+                  <p className="mt-3 text-center text-secondary">Don't have an account ? Click <a onClick={props.handleShowRegister} role="button" className="fw-bold text-dark text-decoration-none">Here</a></p>
+              </Form>
+            </Modal.Body>
+          </Modal>
+        </>
+      );
+}
+
+function RegisterModal(props) {
+    return (
+        <>
+          <Modal show={props.showRegister} onHide={props.handleCloseRegister}>
+            <Modal.Header closeButton>
+              <Modal.Title className="fw-bold">Register</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Control type="email" placeholder="Email" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasictext">
+                  <Form.Control type="text" placeholder="Full Name" />
+                </Form.Group>
+                  <Button variant="danger" className="col-12" onClick={props.handleCloseRegister}>
+                    Submit
+                  </Button>
+                  <p className="mt-3 text-center text-secondary">Already have an account ? Click <a onClick={props.handleShowRegister} role="button" className="fw-bold text-dark text-decoration-none">Here</a></p>
+              </Form>
+            </Modal.Body>
+          </Modal>
+        </>
+      );
+}
+
+export {LoginModal, RegisterModal}
