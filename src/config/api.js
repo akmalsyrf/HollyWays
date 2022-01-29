3cf031f07de59faaadb797f161266e832b275de6
akmalsyrf <dev.akmal69@gmail.com>
2022-01-29 21:50:49 +0700

    config to deploy

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1/",
+  baseURL: process.env.SERVER_URL || "https://api-hollyways.herokuapp.com/api/v1" || "http://localhost:5000/api/v1"
 });
 
 export const setAuthToken = (token) => {
